f02e9cdfc69b96888f122faeaa8428ef536b8ca2
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2021-11-28 17:40:22 +0600

    learned about schema and resolvers using product

--- e-commerce-app/index.js
+++ e-commerce-app/index.js
@@ -1,6 +1,7 @@
 /* This is where our GraphQL server lives */
 const { ApolloServer, gql } = require('apollo-server');
-const productsData = require('./productsData');
+const products = require('./products');
+const categories = require('./categories');
 
 /* define how are query going to look, the way we going to fetch our data */
 /* ! means non nullable, can't return null */
@@ -10,13 +11,21 @@ const typeDefs = gql`
   type Query {
     hello: String
     products: [Product!]!
+    product(id: ID!): Product
+    categories: [Category!]!
+    category(id: ID): Category
   }
   type Product {
     name: String!
     description: String!
     quantity: Int!
     price: Float!
+    image: String!
     onSale: Boolean!
+  }
+  type Category {
+    name: String!
+    products: [Product!]!
   }
 `;
 /* we are gonna resolve the hello property inside our query types */
@@ -28,7 +37,16 @@ const resolvers = {
     },
     products: () => {
       /* object type */
-      return productsData;
+      return products;
+    },
+    product: (parent, args, context) => {
+      const prodcutId = args.id;
+      const product = products.find((product) => product.id === prodcutId);
+      if (!product) return null;
+      return product;
+    },
+    categories: () => {
+      return categories;
     },
   },
 };
@@ -37,5 +55,5 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers }); //our apollo ApolloServer
 
 server.listen().then(({ url }) => {
-  console.log('server is ready at' + url);
+  console.log('server is ready at ' + url);
 });
